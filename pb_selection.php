9cc0a622dc40fd1c001eff0ea26b09b28d7f63fe
Michel Bystranowski <bystrano@gmx.ch>
2013-09-01 19:03:06 +0200

    on ne fait plus l'insertion automatique, on se servira directement le formulaire

--- pb_selection.php
+++ pb_selection.php
@@ -2,36 +2,6 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-
-function pb_selection_interface ( $vars="" ) {
-	$exec = $vars["args"]["exec"];
-	$id_rubrique = $vars["args"]["id_rubrique"];
-	$id_article = $vars["args"]["id_article"];
-	$data =	$vars["data"];
-	
-	if ($id_rubrique < 1) $id_rubrique=0;
-	
-	
-	
-	if ($exec == "rubriques" OR $exec == "rubrique") {
-		
-		$contexte = array('id_rubrique'=>$id_rubrique);
-
-		$ret .= "<div id='pave_selection'>";
-	
-		$page = evaluer_fond("selection_interface_new", $contexte);
-		$ret .= $page["texte"];
-
-		$ret .= "</div>";
-	}
-
-
-	$data = $ret.$data;
-
-	$vars["data"] = $data;
-	return $vars;
-}
-
 function pb_selection_jqueryui_plugins($plugins) {
 	if (_DIR_RACINE == "../") {
 		$plugins[] = "jquery.ui.core";
