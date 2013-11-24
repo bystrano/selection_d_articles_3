c46624db45fce1b23399e678696d78261c9e4c78
Michel Bystranowski <bystrano@gmx.ch>
2013-11-24 11:48:58 +0100

    crée une saisie pour la sélection d'articles

--- formulaires/selection_d_articles.php
+++ formulaires/selection_d_articles.php
@@ -1,5 +1,7 @@
 <?php
 
+include_spip('pb_selection_fonctions');
+
 function formulaires_selection_d_articles_saisies ($id_selection, $nom_form) {
 
     include_spip('inc/yaml');
@@ -11,49 +13,17 @@ function formulaires_selection_d_articles_saisies ($id_selection, $nom_form) {
 
 function formulaires_selection_d_articles_charger_dist ($id_selection, $nom_form) {
 
-  include_spip('base/abstract_sql');
-
-  $selection = sql_allfetsel('id_article', 'spip_pb_selection',
-                             'id_selection=' . sql_quote($id_selection),
-                             '','ordre');
-
-  $valeurs_saisie_selection = array();
-  foreach ($selection as $i => $article) {
-    $valeurs_saisie_selection[$i] = array(
-                                      'article' => 'article|' . $article['id_article']
-                                    );
-  }
-
-  $req = _request('selection');
-
-  $contexte = array(
-                'selection' => $req ? $req : $valeurs_saisie_selection,
-              );
-
-  return $contexte;
+    return selection_d_articles_charger($id_selection);
 }
 
 function formulaires_selection_d_articles_verifier_dist ($id_selection, $nom_form) {
 
-  if (liste_objets_verifier('selection')) return;
+    $erreurs = selection_d_articles_verifier($id_selection);
+
+    return $erreurs;
 }
 
 function formulaires_selection_d_articles_traiter_dist ($id_selection, $nom_form) {
 
-  liste_objets_traiter('selection');
-
-  $selection = _request('selection');
-
-  include_spip('base/abstract_sql');
-
-  sql_delete('spip_pb_selection', 'id_selection='.sql_quote($id_selection));
-
-  foreach ($selection as $i => $article) {
-    $id_article = str_replace('article|', '', $article['article'][0]);
-    sql_insertq('spip_pb_selection', array(
-                                       'id_selection' => $id_selection,
-                                       'id_article'   => $id_article,
-                                       'ordre'        => $i,
-                                     ));
-  }
+    return selection_d_articles_traiter($id_selection);
 }
---
+++ pb_selection_fonctions.php
@@ -0,0 +1,60 @@
+<?php
+if (!defined("_ECRIRE_INC_VERSION")) return;
+
+function selection_d_articles_charger ($id_selection) {
+
+    include_spip('base/abstract_sql');
+
+    $selection = sql_allfetsel('id_article', 'spip_pb_selection',
+                               'id_selection=' . sql_quote($id_selection),
+                               '','ordre');
+
+    $valeurs_saisie_selection = array();
+    foreach ($selection as $i => $article) {
+        $valeurs_saisie_selection[$i] = array(
+            'article' => 'article|' . $article['id_article']
+        );
+    }
+
+    $req = _request('selection');
+
+    $contexte = array(
+        'selection' => $req ? $req : $valeurs_saisie_selection,
+    );
+
+    return $contexte;
+}
+
+function selection_d_articles_verifier ($id_selection) {
+
+    $erreurs = liste_objets_verifier('selection');
+
+    /* Dans le cas où on a cliqué un submit propre à la saisie, on
+       interrompt les traitements du formulaire, mais il faut quand
+       même executer les traitements de la saisie selection_d_articles. */
+    if (array_key_exists('stop', $erreurs)) {
+
+        $selection = _request('selection');
+
+        include_spip('base/abstract_sql');
+
+        sql_delete('spip_pb_selection',
+                   'id_selection='.sql_quote($id_selection));
+
+        foreach ($selection as $i => $article) {
+            $id_article = str_replace('article|', '', $article['article'][0]);
+            sql_insertq('spip_pb_selection', array(
+                'id_selection' => $id_selection,
+                'id_article'   => $id_article,
+                'ordre'        => $i,
+            ));
+        }
+    }
+
+    return $erreurs;
+}
+
+function selection_d_articles_traiter ($id_selection) {
+
+    return liste_objets_traiter('selection');
+}
