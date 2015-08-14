1abe8d5d53931e81af7d6afca4b181d01d41d2b8
Michel Bystranowski <michel@vertige.org>
2015-08-14 15:10:15 +0200

    Pas besoin de charger les plugins de jQueryUI
    le plugin saisie_liste s'en charge…

--- pb_selection.php
+++ pb_selection.php
@@ -2,16 +2,6 @@
 
 if (!defined("_ECRIRE_INC_VERSION")) return;
 
-function pb_selection_jqueryui_plugins($plugins) {
-	if (_DIR_RACINE == "../") {
-		$plugins[] = "jquery.ui.core";
-		$plugins[] = "jquery.ui.widget";
-		$plugins[] = "jquery.ui.mouse";
-		$plugins[] = "jquery.ui.sortable";
-	}
-	return $plugins;
-}
-
 function pb_selection_header_prive ($flux) {
 
   $flux .= '<link rel="stylesheet" href="' . find_in_path('selection_d_articles.css') . '" type="text/css"></link>';
